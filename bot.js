5158e7214ef1c2467f904887ef49d25301ef6aa0
smit1988 <smit1988@purdue.edu>
2016-03-03 00:53:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,7 +85,7 @@ function respond() {
   } 
   else if(request.text && botDuck.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("no");
+    postMessage("http://media3.giphy.com/media/YCseTHF2I6CCA/giphy.gif");
     this.res.end();
   }
   else if(request.text && botRegexCC.test(request.text)) {
