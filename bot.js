9ff9c6d970d86d91128b1de4db0f1c35cb31e407
smit1988 <smit1988@purdue.edu>
2016-03-03 20:25:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegexName = /^\Noah Smith/;  botRegex = /^\/cool guy/;  botRegexBall = /^\/8ball/;  botRegexSpook = /^\/spooky/;  botRegexDie = /^\/roll a die/;  botRegexPic = /^\/pic/; botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
+      botRegex = /^\/cool guy/;  botRegexBall = /^\/8ball/;  botRegexSpook = /^\/spooky/;  botRegexDie = /^\/roll a die/;  botRegexPic = /^\/pic/; botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/;
@@ -112,7 +112,7 @@ function respond() {
     postMessage("https://media.giphy.com/media/1bgaiT0clnf0I/giphy.gif");
     this.res.end();
   }
-  else if(request.name && botRegexName.test(request.text)) {
+  else if(request.name == "Noah Smith") {
     this.res.writeHead(200);
     postMessage("Ryan is uncool\nType /cool guy to mock Ryan");
     this.res.end();
